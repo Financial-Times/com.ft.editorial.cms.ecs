4615c6da9f6fa60cc7ae3f443adfd81b026ca3a2
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-10-18 15:21:38 +0100

    update tasks/echoes/echoes-service-alb.yaml

--- tasks/echoes/echoes.sh
+++ tasks/echoes/echoes.sh
@@ -4,5 +4,5 @@
 # "Time now on host <hostname>: <date>"
 # when HTTP client makes request
 while true; do
-  echo "Time now on host $(hostname): $(date)." | nc -l -p 8080
+  (echo -e 'HTTP/1.1 200 OK\r\n'; echo -e "\n\tVERSION:\t 5" ; echo -e "\tCONTAINER ID:\t $(hostname)" ; echo -e "\tTIME:\t\t $(date)\n") | nc -l -p 8080;
 done
