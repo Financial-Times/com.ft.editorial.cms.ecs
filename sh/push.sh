6aa88c5970b8541fd6bb917cc943b207140e87f5
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-11-16 11:36:13 +0000

    Add logging to  push.sh

--- sh/push.sh
+++ sh/push.sh
@@ -14,28 +14,30 @@ AWS_ACCOUNT_ID=${3:-"307921801440"}
 REGION=${4:-"eu-west-1"}
 
 install_aws_cli() {
-  ## Push container image to ECR
-  # Update pip & awscli
+  echo "Update pip & awscli"
   pip install --upgrade pip
   pip install --upgrade awscli
-  aws configure set default.region ${AWS_REGION}
 }
 
 # Check whether to install aws clis
 which aws >/dev/null || install_aws_cli
 
-# Login into ECR
+echo "Set AWS region"
+aws configure set default.region ${AWS_REGION}
+
+
+echo "Login to ECR"
 $(aws ecr get-login --no-include-email)
 
-# Verify repository
+echo "Verify repository exists"
 aws ecr describe-repositories --repository-names ${IMAGE_NAME} >/dev/null || \
 aws ecr create-repository --repository-name ${IMAGE_NAME}}
 
-# Tagging container
+echo "Tag image"
 docker tag ${IMAGE_NAME}:${IMAGE_VERSION} \
   ${ECR_ENDPOINT}/${IMAGE_NAME}:${IMAGE_VERSION}
 docker tag ${ECR_ENDPOINT}/${IMAGE_NAME}:${IMAGE_VERSION} \
   ${ECR_ENDPOINT}/${IMAGE_NAME}:latest
 
-# Pushing container to ECR
+echo "Pushing container to ECR"
 docker push ${ECR_ENDPOINT}/${IMAGE_NAME}
