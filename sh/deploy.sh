2c3188d522975868429a214000e6f060cf2d4d0e
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-11-30 09:26:57 +0000

    Introduce sh/common.sh for common functions

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -8,17 +8,22 @@
 # Script is based on https://github.com/circleci/go-ecs-ecr/blob/master/deploy.sh
 #
 # USAGE: deploy.sh <ecs_cluster> <ecs_service> [image_name] [image_version] [aws_account_id] [region]
+#   OR
+# USAGE: deploy.sh --ecs_cluster=cluster_name --ecs_service=service_name [--image_name=image_name] [--image_version=image_version] [--aws_account_id=aws_account_id] [--aws_region=aws_region]
 #
 
-CLUSTER=$1
-SERVICE=$2
-IMAGE_NAME=${3:-${CIRCLE_PROJECT_REPONAME}}
-IMAGE_VERSION=${4:-${CIRCLE_BUILD_NUM}}
-AWS_ACCOUNT_ID=${5:-"307921801440"}
-REGION=${6:-"eu-west-1"}
+source $(dirname $0)/common.sh || echo "$0: Failed to source common.sh"
+processCliArgs $@
+
+test -z ${ARGS[--cluster_name]} && ARGS[--cluster_name]=$1
+test -z ${ARGS[--ecs_service]} && ARGS[--ecs_service]=$2
+test -z ${ARGS[--image_name]} && ARGS[--image_name]=${3:-${CIRCLE_PROJECT_REPONAME}}
+test -z ${ARGS[--image_version]} && ARGS[--image_version]=${4:-${CIRCLE_BUILD_NUM}}
+test -z ${ARGS[--aws_account_id]} && ARGS[--aws_account_id]${5:-"307921801440"}
+test -z ${ARGS[--aws_region]} && ARGS[--aws_region]=${6:-"eu-west-1"}
 
 deploy() {
-    if [[ $(aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} --task-definition $revision \
+    if [[ $(aws ecs update-service --cluster ${ARGS[--cluster_name]} --service ${ARGS[--ecs_service]} --task-definition $revision \
             --output text --query 'service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
@@ -41,12 +46,12 @@ make_task_definition(){
 		}
 	]'
 
-	task_def=$(printf "$task_template" ${SERVICE} ${AWS_ACCOUNT_ID} ${REGION} ${IMAGE_NAME} ${IMAGE_VERSION})
+	task_def=$(printf "$task_template" ${ARGS[--ecs_service]} ${ARGS[--aws_account_id]} ${ARGS[--aws_region]} ${ARGS[--image_name]} ${ARGS[--image_version]}})
 }
 
 register_task_definition() {
     echo "Registering task definition ${task_def}"
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family "${SERVICE}" --output text --query 'taskDefinition.taskDefinitionArn'); then
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family "${ARGS[--ecs_service]}" --output text --query 'taskDefinition.taskDefinitionArn'); then
         echo "Revision: $revision"
     else
         echo "Failed to register task definition"
--- sh/push.sh
+++ sh/push.sh
@@ -6,12 +6,19 @@
 # unless passed in as command line parameter
 #
 #
-# USAGE: ./push.sh [image_name] [image_version] [aws_account_id] [region]
+# USAGE: ./push.sh [image_name] [image_version] [aws_account_id] [aws_region]
+#   OR
+# USAGE: ./push.sh --image_name=image_name --image_version=image_version --aws_account_id=aws_account_id --aws_region=aws_region --ecr_endpoint=ecr-endpoint
 
-IMAGE_NAME=${1:-${CIRCLE_PROJECT_REPONAME}}
-IMAGE_VERSION=${2:-${CIRCLE_BUILD_NUM}}
-AWS_ACCOUNT_ID=${3:-"307921801440"}
-REGION=${4:-"eu-west-1"}
+source $(dirname $0)/common.sh || echo "$0: Failed to source common.sh"
+
+processCliArgs $@
+
+test -z ${ARGS[--image_name]} && ARGS[--image_name]=${1:-${CIRCLE_PROJECT_REPONAME}}
+test -z ${ARGS[--image_version]} && ARGS[--image_version]=${2:-${CIRCLE_BUILD_NUM}}
+test -z ${ARGS[--aws_account_id]} && ARGS[--aws_account_id]=${3:-"307921801440"}
+test -z ${ARGS[--aws_region]} && ARGS[--aws_region]=${4:-"eu-west-1"}
+test -z ${ARGS[--ecr_endpoint]} && ARGS[--ecr_endpoint]="307921801440.dkr.ecr.eu-west-1.amazonaws.com"
 
 install_aws_cli() {
   pip install --upgrade pip
@@ -19,24 +26,23 @@ install_aws_cli() {
 }
 
 # Check whether to install aws clis
-install_aws_cli
+which aws &>/dev/null || install_aws_cli
 
 echo "Set AWS region"
-aws configure set default.region ${AWS_REGION}
-
+aws configure set default.region ${ARGS[--aws_region]}
 
 echo "Login to ECR"
 $(aws ecr get-login --no-include-email)
 
 echo "Verify repository exists"
-aws ecr describe-repositories --repository-names ${IMAGE_NAME} >/dev/null || \
-aws ecr create-repository --repository-name ${IMAGE_NAME}}
+aws ecr describe-repositories --repository-names ${ARGS[--image_name]}} &>/dev/null || \
+aws ecr create-repository --repository-name ${ARGS[--image_name]}
 
 echo "Tag image"
-docker tag ${IMAGE_NAME}:${IMAGE_VERSION} \
-  ${ECR_ENDPOINT}/${IMAGE_NAME}:${IMAGE_VERSION}
-docker tag ${ECR_ENDPOINT}/${IMAGE_NAME}:${IMAGE_VERSION} \
-  ${ECR_ENDPOINT}/${IMAGE_NAME}:latest
+docker tag ${ARGS[--image_name]}:${IMAGE_VERSION} \
+  ${ARGS[--ecr_endpoint]}/${ARGS[--image_name]}:${ARGS[--image_version]}
+docker tag ${ECR_ENDPOINT}/${ARGS[--image_name]}:${ARGS[--image_version]} \
+  ${ARGS[--ecr_endpoint]}/${ARGS[--image_name]}:latest
 
 echo "Pushing container to ECR"
-docker push ${ECR_ENDPOINT}/${IMAGE_NAME}
+docker push ${ARGS[--ecr_endpoint]}/${ARGS[--image_name]}
