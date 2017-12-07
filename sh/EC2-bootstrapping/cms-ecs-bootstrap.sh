e2eb90a59fe0f4192b1be22cfd34cb5e1131abcf
Pawel Zakiewicz <pawel.zakiewicz@ft.com>
2017-12-07 12:22:24 +0000

    Adding extra verbosity to bootstrap script & adding sctipt to EC2 user data

--- sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
+++ sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
@@ -20,22 +20,36 @@ case $1 in
 esac
 
 #Adding tomcat user (CMT-1909)
+echo "Adding tomcat user"
 adduser -c \"Tomcat user\" -d /var/log/apps -M -s /sbin/nologin -u 1000 -U tomcat
+id tomcat
+echo
 
 #Adding container logging locations (CMT-1910)
+echo "Adding container logs locations"
 for s in restapi mis rhelper preview webclient checkin formats mfm image datasource prodarch portalpub postprint eventhandler adorder mms mss msis
 do
-  mkdir /var/log/apps/methode-$s
-  chown tomcat. /var/log/apps/methode-$s
+  mkdir -v /var/log/apps/methode-$s
+  chown -v tomcat. /var/log/apps/methode-$s
 done
+echo
 
 #Adding logging drivers to ECS config (CMT-1949)
+echo "Adding logging drivers to ECS config"
 echo 'ECS_AVAILABLE_LOGGING_DRIVERS= ["json-file","awslogs","splunk"]' >> /etc/ecs/ecs.config
+cat /etc/ecs/ecs.config
+echo
 
 #Updating Splunk collector configuration (CMT-1890)
-cp /opt/splunkforwarder/etc/system/local/props.conf{,bck}
+echo "Updating Splunk collector configuration"
+cp -v /opt/splunkforwarder/etc/system/local/props.conf{,.bck}
 sed -i -e '\#\[source::.../log/(apps|apps/...|restricted/\*/apps|restricted/\*/apps/...)/\*\.log\]#{n; s/\(sourcetype = \)log4j/\1access_combined_time/}' /opt/splunkforwarder/etc/system/local/props.conf
-cp /opt/splunkforwarder/etc/system/local/inputs.conf{,.bck}
+diff -U0 /opt/splunkforwarder/etc/system/local/props.conf{.bck,}
+echo
+cp -v /opt/splunkforwarder/etc/system/local/inputs.conf{,.bck}
 sed -i -e '\#\[monitor:///var/log/apps\]#,+4s/\(whitelist = \).*/\1tomcat_access_.\*\\.log\$/' /opt/splunkforwarder/etc/system/local/inputs.conf
 sed -i -e "\#\[monitor:///var/log/apps\]#,+4s/\(index = \).*/\1cms-ecs_$ENV/" /opt/splunkforwarder/etc/system/local/inputs.conf
+diff -U0 /opt/splunkforwarder/etc/system/local/inputs.conf{.bck,}
+echo
 
+echo "CMS ECS customisation DONE"
