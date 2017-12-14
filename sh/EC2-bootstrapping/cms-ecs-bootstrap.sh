ab953f960d87b3ca66d3a6ebac9f43705ad5c730
Pawel Zakiewicz <pawel.zakiewicz@ft.com>
2017-12-14 16:31:45 +0000

    Adding bouncing ecs agent

--- sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
+++ sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
@@ -39,6 +39,7 @@ echo "Adding logging drivers to ECS config"
 echo 'ECS_AVAILABLE_LOGGING_DRIVERS= ["json-file","awslogs","splunk"]' >> /etc/ecs/ecs.config
 cat /etc/ecs/ecs.config
 echo
+stop ecs && start ecs
 
 #Updating Splunk collector configuration (CMT-1890)
 echo "Updating Splunk collector configuration"
