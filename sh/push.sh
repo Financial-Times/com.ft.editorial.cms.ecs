6c784f18991d4cfb24129730f069c7311336a7b3
Jussi.Heinonen <jussi.heinonen@ft.com>
2018-01-30 14:44:37 +0000

    Add Usage to push.sh

--- sh/push.sh
+++ sh/push.sh
@@ -8,22 +8,31 @@
 #
 # USAGE: ./push.sh [image_name] [image_version] [aws_account_id] [aws_region]
 #   OR
-# USAGE: ./push.sh --image_name=image_name --image_version=image_version --aws_account_id=aws_account_id --aws_region=aws_region --ecr_endpoint=ecr-endpoint
+# USAGE: ./push.sh --image_name=image_name --image_version=image_version [--aws_account_id=aws_account_id] [--aws_region=aws_region] [--ecr_endpoint=ecr-endpoint]
 
 source $(dirname $0)/common.sh || echo "$0: Failed to source common.sh"
 
 processCliArgs $@
 
+usage() {
+  echo "USAGE: $0 --image_name=image_name --image_version=image_version [--aws_account_id=aws_account_id] [--aws_region=aws_region] [--ecr_endpoint=ecr-endpoint]"
+  exit 0
+}
+
 test -z ${ARGS[--image_name]} && ARGS[--image_name]=${1:-${CIRCLE_PROJECT_REPONAME}}
 test -z ${ARGS[--image_version]} && ARGS[--image_version]=${2:-${CIRCLE_BUILD_NUM}}
 test -z ${ARGS[--aws_account_id]} && ARGS[--aws_account_id]=${3:-"307921801440"}
 test -z ${ARGS[--aws_region]} && ARGS[--aws_region]=${4:-"eu-west-1"}
-test -z ${ARGS[--ecr_endpoint]} && ARGS[--ecr_endpoint]="307921801440.dkr.ecr.eu-west-1.amazonaws.com"
+test -z ${ARGS[--ecr_endpoint]} && ARGS[--ecr_endpoint]="${ARGS[--aws_account_id]}.dkr.ecr.${ARGS[--aws_region]}.amazonaws.com"
 
 install_aws_cli() {
   pip install --upgrade pip
   pip install --upgrade awscli
 }
+
+test -z ${ARGS[--image_name]} && usage
+test -z ${ARGS[--image_version]} && usage
+
 
 # Check whether to install aws clis
 which aws &>/dev/null || install_aws_cli
