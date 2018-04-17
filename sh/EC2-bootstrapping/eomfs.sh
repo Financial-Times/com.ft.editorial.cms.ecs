3f6cb6675802b4698031d2c997b28676baa21b0c
Jussi.Heinonen <jussi.heinonen@ft.com>
2018-04-17 11:34:33 +0100

    Add function addCron to eomfs.sh to improve NFS performance

--- sh/EC2-bootstrapping/eomfs.sh
+++ sh/EC2-bootstrapping/eomfs.sh
@@ -3,7 +3,7 @@
 # Configure autofs for eomfs NFS shares
 #
 # Maintainer: jussi.heinonen@ft.com
-# Date: 26.1.2018
+# Date: 17.4.2018
 
 . $(dirname $0)/functions.sh
 
@@ -20,6 +20,15 @@ declare -a APPS=( "wires" "staging" "archive" )
 # Set environment dev in case it's not been set
 test -z $ENV && export ENV="dev"
 
+addCron() {
+  # $1 is the directory to mount, e.g. /var/lib/eomfs/wires
+  MOUNTPOINT="$1"
+  # CRON_MIN is is NFS_TIMEOUT in minutes plus 1 minute, allow autofs mount to timeout before remounting
+  CRON_MIN=$(expr ${NFS_TIMEOUT} / 60 + 1)
+  # Append on crontab
+  (crontab -l ; echo "*/${CRON_MIN} * * * * (cd ${MOUNTPOINT})") | crontab -
+}
+
 info "environment is $ENV"
 
 # Reset master file
@@ -30,6 +39,7 @@ for each in ${APPS[*]}; do
   mkdir -p /var/lib/eomfs/${each}
   echo "/- /etc/auto.master.d/auto.eomfs.${each} --timeout=${NFS_TIMEOUT} --verbose" >> ${AUTOFS_MASTER}
   echo "/var/lib/eomfs/${each} -rw,soft,intr,bg,retrans=1,retry=0,noatime,nodiratime,timeo=${NFS_TIMEOUT} ${each}.${NFS_SERVER[${ENV}]}:/var/lib/eomfs" > /etc/auto.master.d/auto.eomfs.${each}
+  addCron /var/lib/eomfs/${each}
 done
 
 info "$0: Restarting autofs"
