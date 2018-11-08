843f10175374aecced73fa844cf8142768cc334f
Pawel Zakiewicz <pawel.zakiewicz@ft.com>
2018-11-08 14:23:07 +0000

    Output manager logs location

--- sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
+++ sh/EC2-bootstrapping/cms-ecs-bootstrap.sh
@@ -35,7 +35,7 @@ echo
 
 #Adding container logging locations (CMT-1910)
 echo "Adding container logs locations"
-for s in restapi wires-mis staging-mis wires-mfm staging-mfm rhelper preview webclient checkin formats mfm image datasource prodarch portalpub postprint eventhandler adorder mms mss msis
+for s in restapi wires-mis staging-mis wires-mfm staging-mfm rhelper preview webclient checkin formats mfm image datasource prodarch portalpub postprint eventhandler adorder mms mss msis outputmanager
 do
   mkdir -v /var/log/apps/methode-$s
   chown -v tomcat. /var/log/apps/methode-$s
