a4ccc68137320deaebc6e751928420fc286b3f20
Jussi Heinonen <jussi.heinonen@ft.com>
2020-07-29 14:00:28 +0100

    restore the mess I created with wrong git command parameters

---
+++ sh/EC2-bootstrapping/cms-integration-bootstrap.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Bootstrap routine for ECS instance
+#
+# When script has been modified upload it https://s3-eu-west-1.amazonaws.com/cms-tech-s3/ECS-bootstrap/cms-integration-bootstrap.sh
+
+#Workout enviroment name
+case $1 in
+	"p")
+		export ENV="prod"
+		;;
+	"int")
+		export ENV="int"
+		;;
+	"t")
+		export ENV="test"
+		;;
+	"d")
+		export ENV="dev"
+		;;
+	"*")
+		export ENV="dev"
+		;;
+esac
+
+#Adding logging drivers to ECS config (CMT-1949)
+echo "Adding logging drivers to ECS config"
+echo 'ECS_AVAILABLE_LOGGING_DRIVERS= ["json-file","awslogs","splunk"]' >> /etc/ecs/ecs.config
+cat /etc/ecs/ecs.config
+echo
+stop ecs && start ecs
+
+#Updating Splunk collector configuration (CMT-1890)
+echo "Updating Splunk collector configuration"
+cp -v /opt/splunkforwarder/etc/system/local/props.conf{,.bck}
+echo -e "\n[source::.../log/apps/*/tomcat_access_*.log]\nsourcetype = access_combined_time\npriority = 99" >> /opt/splunkforwarder/etc/system/local/props.conf
+diff -U0 /opt/splunkforwarder/etc/system/local/props.conf{.bck,}
+echo
+cp -v /opt/splunkforwarder/etc/system/local/inputs.conf{,.bck}
+sed -i -e "\#\[monitor:///var/log/apps\]#,+4s/\(index = \).*/\1cms-ecs_$ENV/" /opt/splunkforwarder/etc/system/local/inputs.conf
+diff -U0 /opt/splunkforwarder/etc/system/local/inputs.conf{.bck,}
+echo
+
+#Restaring Splunk collector
+/etc/init.d/splunk restart
+echo
+
+#Pull common functions
+aws s3 cp s3://cms-tech-s3/ECS-bootstrap/functions.sh ./
+
+#Set FT nameservers
+aws s3 cp s3://cms-tech-s3/ECS-bootstrap/nameservers.sh ./
+. ./nameservers.sh
+
+#Set up FSx mount
+aws s3 cp s3://cms-tech-s3/ECS-bootstrap/ftfsx.sh ./
+. ./ftfsx.sh
+
+#Add hanging network share revival script on cron
+aws s3 cp s3://cms-tech-s3/ECS-bootstrap/network-share-revival-ftfsx.sh ./
+(crontab -l ; echo "*/2 * * * * bash /network-share-revival-ftfsx.sh") | crontab -
+
+echo "ECS customisation DONE"
---
+++ sh/EC2-bootstrapping/network-share-revival-ftfsx.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#
+# Detect hanging network shares and recover them.
+# USAGE: ./network-share-revival-ftfsx.sh
+
+info() {
+  echo -e "\e[34m${0}: ${1}\e[0m"
+  logger ${0}: ${1}
+}
+
+error() {
+  echo -e "\e[31m${0}: $1\e[0m"
+  logger ${0}: ${1}
+}
+
+checkForHangingShares() {
+  # Check process list and identify hanging cd commands for network shares
+  PATTERN="/bin/bash -c (cd /var/lib"
+  PROC_COUNT="$(ps -ef | grep "${PATTERN}" | grep -v grep | wc -l)"
+  if [[ "${PROC_COUNT}" -gt "0" ]]; then
+    for each in $(ps -ef | grep "${PATTERN}" | grep -v grep | awk '{print $11}' | tr -d ')' | sort | uniq ); do
+      info "Trying to recover network share ${each}"
+      unmountShare ${each}
+      killHangingProcesses ${each}
+      /etc/init.d/autofs restart && info "Autofs service restarted"
+      (cd ${each})
+    done
+  else
+    info "No hanging network shares detected"
+  fi
+}
+
+killHangingProcesses() {
+  SHARE="$1"
+  info "Killing hanging processes for ${SHARE}"
+  ps -ef | grep "/bin/bash -c (cd ${SHARE})" | grep -v grep | awk '{print $2}' | xargs kill -9
+}
+
+unmountShare() {
+  # Lookup share type (nfs4 or cifs) and unmount it
+  SHARE="$1"
+  SHARE_TYPE="$(mount | grep ${SHARE} | grep -v autofs | awk '{print $5}')"
+  if [[ -z "${SHARE_TYPE}" ]]; then
+    info "Unable to resolve mount type for share ${SHARE}. Assuming it is nfs"
+    SHARE_TYPE="nfs"
+  fi
+  umount -f -a -t ${SHARE_TYPE} -l ${SHARE} && umount -i -f -l ${SHARE}
+  info "umount commands issued for share ${SHARE}"
+
+}
+
+# ENTRYPOINT
+sleep $(( ( RANDOM % 10 )  + 1 )) # Add a few random sleep seconds to avoid clash with mounting tasks
+checkForHangingShares
+
+# Checking for gone away mounts
+
+for m in ftfsx
+do
+  mount_test=$(grep -c $m /proc/mounts)
+  if [ "$mount_test" -eq 0 ]
+  then
+    info "$m mount gone away, restarting autofs"
+    /etc/init.d/autofs restart && info "Autofs service restarted"
+    exit 0
+  else
+    info "$m mount seems fine"
+  fi
+done
